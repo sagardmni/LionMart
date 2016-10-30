c6907311184570f7bac9b4a4f20de2c51f288d03
Abhijeet Mehrotra <abhijeet.meh@gmail.com>
2016-10-29 21:45:53 -0400

    class definitions

--- code/app/models/Product.java
+++ code/app/models/Product.java
@@ -7,13 +7,132 @@ import java.util.Date;
  */
 public class Product {
     private long id;
+    private long uploadedBy;
     private String imagePath;
-    private int price;
+    private float price;
     private String description;
     private Date dateUploaded;
     private Date dateSold;
+    private int priceBought;
     private String onlineLink;
-    private int soldPrice;
+    private float soldPrice;
+    private int condition;
+    private int months;
 
+    public Product() {
 
+    }
+
+    public Product(long id, long uploadedBy, String imagePath, float price, String description,
+                   Date dateUploaded, Date dateSold, int priceBought, String onlineLink,
+                   float soldPrice, int condition, int months) {
+        this.id = id;
+        this.uploadedBy = uploadedBy;
+        this.imagePath = imagePath;
+        this.price = price;
+        this.description = description;
+        this.dateUploaded = dateUploaded;
+        this.dateSold = dateSold;
+        this.priceBought = priceBought;
+        this.onlineLink = onlineLink;
+        this.soldPrice = soldPrice;
+        this.condition = condition;
+        this.months = months;
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateUploaded() {
+        return dateUploaded;
+    }
+
+    public void setDateUploaded(Date dateUploaded) {
+        this.dateUploaded = dateUploaded;
+    }
+
+    public Date getDateSold() {
+        return dateSold;
+    }
+
+    public void setDateSold(Date dateSold) {
+        this.dateSold = dateSold;
+    }
+
+    public String getOnlineLink() {
+        return onlineLink;
+    }
+
+    public void setOnlineLink(String onlineLink) {
+        this.onlineLink = onlineLink;
+    }
+
+    public float getSoldPrice() {
+        return soldPrice;
+    }
+
+    public void setSoldPrice(float soldPrice) {
+        this.soldPrice = soldPrice;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public void setMonths(int months) {
+        this.months = months;
+    }
+    public int getPriceBought() {
+        return priceBought;
+    }
+
+    public void setPriceBought(int priceBought) {
+        this.priceBought = priceBought;
+    }
+
+    public long getUploadedBy() {
+        return uploadedBy;
+    }
+
+    public void setUploadedBy(long uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
 }
--- code/app/models/User.java
+++ code/app/models/User.java
@@ -4,4 +4,61 @@ package models;
  * Created by akshay on 10/29/2016.
  */
 public class User {
+
+    private long fbId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private boolean type;
+
+    public User() {
+    }
+
+    public User(long fbId, String firstName, String lastName, String email, boolean type) {
+        this.fbId = fbId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.type = type;
+    }
+
+    public long getFbId() {
+        return fbId;
+    }
+
+    public void setFbId(long fbId) {
+        this.fbId = fbId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isType() {
+        return type;
+    }
+
+    public void setType(boolean type) {
+        this.type = type;
+    }
 }
